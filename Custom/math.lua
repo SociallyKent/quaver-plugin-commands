0df33b8fdf48cce74eda7a63f86ddabb41e0f5e0
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-02-24 20:38:17 -0600

    Update math.lua

--- Custom/math.lua
+++ Custom/math.lua
@@ -3,48 +3,6 @@
 	m, n, o, p, q, r,
 	s, t, u, v, w, x,
 	y, z
---- Duplicate a TABLE
---TABLE table
- function table.duplicate(TABLE)
-	if not TABLE then return {} end
-	local Table = {}
-	for i = 1, #TABLE do
-		local Value = TABLE[i]
-		Table[#Table + 1] = type(Value) == "table" and table.duplicate(Value) or Value
-	end
-    return Table
-end
---- Remove any duplicate values from TABLE
---TABLE table
-function table.duplicate_Remove(TABLE)
-	local Holder, Table = {}, {}
-	for i = 1, #TABLE do
-		local Value = TABLE[i]
-		if not (Holder[Value]) then
-			Table[#Table + 1] = Value
-			Holder[Value] = true
-		end
-	end
-	return Table
-end
---- Find any matches of VALUE inside TABLE
---TABLE table
---VALUE varable
-function table.match(TABLE, VALUE)
-	for i = 1, #TABLE do
-		if TABLE[i] == VALUE then return true end
-	end
-	return false
-end
---- Reverse a TABLE
---TABLE table
-function table.reverse(TABLE)
-	local Table = {}
-	for i = 1, #TABLE do
-		Table[#Table + 1] = TABLE[#TABLE + 1 - i]
-	end
-	return Table
-end
 --- Round NUMBER to decimal point DECIMAL
 --NUMBER numeric
 --DECIMAL numeric
@@ -59,5 +17,5 @@ end
 function math.wrap(NUMBER1, NUMBER2)
 	local a, b = math.modf(NUMBER1/NUMBER2)
 	return NUMBER2 * b, a
-
 end
+
