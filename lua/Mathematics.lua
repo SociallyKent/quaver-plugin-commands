77f1c82b0a43a24ca4e0e9fd2e04c4cbd980abc3
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-02-24 20:05:24 -0600

    Update Mathematics.lua

--- lua/Mathematics.lua
+++ lua/Mathematics.lua
@@ -9,13 +9,13 @@ math.abs(x) -- absolute
 math.acos(x) -- arc cosine
 math.cos(x) -- cosine
 math.cosh(x) -- hyperbolic cosine
+math.asin(x) -- arc sine
 math.sin(x) -- sine
 math.sinh(x) -- hyperbolic sine
-math.asin(x) -- arc sine
+math.atan(x) -- arc tangent
+math.atan2(y, x) ---
 math.tan(x) -- tangent
 math.tanh(x) -- hyperbolic tangent
-math.atan(x) -- arc tangent
-math.atan2(y, x)
 math.ceil(x)--(x, ...)-- returns the first value
 math.floor(x)--(x, ...)-- returns the last value
 math.max(x)--(x, ...)-- returns the highest value
@@ -29,7 +29,8 @@ math.pi -- represents π
 math.modf(x) -- returns whole value aswell as the fractional value
   --example: if x = 2.56, it returns 2 and 0.56. if x = 7, it returns 7 and 0, because 7 has no number in a decimal point.
 math.fmod(x, y) -- x = dividend, y = divisor/modulus 
-  --example: if x = 4 and y = 2, it returns a whole number (0), because 4 is a multiple of two; if x = 4 and y = 3, it returns 1, because 4 is not a multiple of 3
+  --example: if x = 499, y = 360, it returns 139 because 499 cannot fit into 360 so it wrapped it around until it could. (does not return amount of wraps needed)
+  -- fmod is not a wrapper though! x - trunc(x / y) * y, where trunc removes the fractional part toward zero
 math.frexp(x)
 math.ldexp(m, e) -- returns m2^e
 math.log(x, base) -- logarithm -- if no base is given; defaults to e
