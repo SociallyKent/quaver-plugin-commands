1100df74d83cb27c5b10db39883e465eab79f042
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-02-26 12:20:28 -0600

    mixc

---
+++ imgui/imguiNET/Containers/Combo/Flags.lua
@@ -0,0 +1,7 @@
+imgui_combo_flags.None--alt:1
+imgui_combo_flags.HeightSmall--alt:2  -- Max ~4 items visible
+imgui_combo_flags.HeightRegular--alt:4  -- Max ~8 items visible 
+imgui_combo_flags.HeightLarge--alt:8  -- Max ~20 items visible
+imgui_combo_flags.HeightLargest--alt:16  -- As many items visible as possible
+imgui_combo_flags.NoArrowButton--alt:32  -- Display only preview
+imgui_combo_flags.NoPreview--alt:16  -- Display only arrow button
