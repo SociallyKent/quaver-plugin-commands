3c380988d1ab85dd7ca694dd694bdd81877036c7
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-02-27 13:47:10 -0600

    Update and rename Flags to Flags.lua

---
+++ imgui/imguiNET/Window/Flags.lua
@@ -0,0 +1,23 @@
+	imgui_window_flags.NoTitleBar--Alt:1
+	imgui_window_flags.NoResize--Alt:2
+	imgui_window_flags.NoMove--Alt:4
+	imgui_window_flags.NoScrollbar--Alt:8
+	14 -- Always show vertical scrollbar
+	15 -- Show second resize option (has new function)
+	imgui_window_flags.NoScrollWithMouse--Alt:16
+	imgui_window_flags.NoCollapse--Alt:32
+	imgui_window_flags.NoDecoration--Alt:43
+	imgui_window_flags.AlwaysAutoResize--Alt:64
+	imgui_window_flags.NoBackground--Alt:128
+	imgui_window_flags.NoSavedSettings--Alt:256
+	imgui_window_flags.NoMouseInputs--Alt:512
+	imgui_window_flags.MenuBar--Alt:1024
+	imgui_window_flags.HorizontalScrollbar--Alt:2048
+	imgui_window_flags.NoFocusOnAppearing--Alt:4096
+	imgui_window_flags.NoBringToFrontOnFocus--Alt:8192
+	imgui_window_flags.NoNavInputs--Alt:65536
+	imgui_window_flags.NoNavFocus--Alt:131072
+	imgui_window_flags.NoNav--Alt:196608
+	imgui_window_flags.NoInputs--Alt:197120
+	imgui_window_flags.UnsavedDocument--Alt:262144
+	imgui_window_flags.ChildMenu--Alt:26843546
