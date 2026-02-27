534e4fcb2b17b7945b4b99f7f6839971f7e40370
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-02-27 13:44:43 -0600

    Update Flags.lua

--- imgui/imguiNET/Tables/Flags.lua
+++ imgui/imguiNET/Tables/Flags.lua
@@ -1,10 +1,10 @@
 imgui_table_flags.None--alt:0
-imgui_table_flags.Resizable--alt:1
-imgui_table_flags.Reorderable--alt:2
-imgui_table_flags.Hideable--alt:4
-imgui_table_flags.Sortable--alt:8
-imgui_table_flags.NoSavedSettings--alt:16
-imgui_table_flags.ContextMenuInBody--alt:32
+imgui_table_flags.Resizable--alt:1 -- Enable resizing columns.
+imgui_table_flags.Reorderable--alt:2 -- Enable reordering columns in header row (need calling TableSetupColumn() + TableHeadersRow() to display headers)
+imgui_table_flags.Hideable--alt:4 -- Enable hiding/disabling columns in context menu.
+imgui_table_flags.Sortable--alt:8 -- Enable sorting. Call TableGetSortSpecs() to obtain sort specs.
+imgui_table_flags.NoSavedSettings--alt:16 -- Disable persisting columns order, width and sort settings in the .ini file.
+imgui_table_flags.ContextMenuInBody--alt:32 -- Right-click on columns body/contents will display table context menu. By default it is available in TableHeadersRow().
 imgui_table_flags.RowBg--alt:64
 imgui_table_flags.BordersInnerH--alt:128
 imgui_table_flags.BordersOuterH--alt:256
@@ -22,16 +22,16 @@ imgui_table_flags.SizingFixedSame--alt:16384
 imgui_table_flags.SizingStretchProp--alt:24576
 imgui_table_flags.SizingStretchSame--alt:32768
 imgui_table_flags.SizingMask--alt:57344
-imgui_table_flags.NoHostExtendX--alt:65536
-imgui_table_flags.NoHostExtendY--alt:131072
-imgui_table_flags.NoKeepColumnsVisible--alt:262144
+imgui_table_flags.NoHostExtendX--alt:65536 -- Make outer width auto-fit to columns, overriding outer_size.x value. Only available when ScrollX/ScrollY are disabled and Stretch columns are not used.
+imgui_table_flags.NoHostExtendY--alt:131072 -- Make outer height stop exactly at outer_size.y (prevent auto-extending table past the limit). Only available when ScrollX/ScrollY are disabled. Data below the limit will be clipped and not visible.
+imgui_table_flags.NoKeepColumnsVisible--alt:262144 -- Disable keeping column always minimally visible when ScrollX is off and table gets too small. Not recommended if columns are resizable.
 imgui_table_flags.PreciseWidths--alt:524288
-imgui_table_flags.NoClip--alt:1048576
+imgui_table_flags.NoClip--alt:1048576 -- Disable clipping rectangle for every individual columns (reduce draw command count, items will be able to overflow into other columns). Generally incompatible with TableSetupScrollFreeze().
 imgui_table_flags.PadOuterX--alt:2097152
 imgui_table_flags.NoPadOuterX--alt:4194304
 imgui_table_flags.NoPadInnerX--alt:8388608
 imgui_table_flags.ScrollX--alt:16777216
 imgui_table_flags.ScrollY--alt:33554432
-imgui_table_flags.SortMulti--alt:67108864
-imgui_table_flags.SortTristate--alt:134217728
+imgui_table_flags.SortMulti--alt:67108864 -- Hold shift when clicking headers to sort on multiple column. TableGetSortSpecs() may return specs where (SpecsCount > 1).
+imgui_table_flags.SortTristate--alt:134217728 -- Allow no sorting, disable default sorting. TableGetSortSpecs() may return specs where (SpecsCount == 0).
 imgui_table_flags.HighlightHoveredColumn--alt:268435456
