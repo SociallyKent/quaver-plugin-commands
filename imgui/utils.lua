fbb213f75bcc1d548b3cf5516da2b6562fb602da
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-03-06 20:57:58 -0600

    ww

---
+++ imgui/utils.lua
@@ -0,0 +1,4 @@
+imgui.ColorConvertFloat4ToU32(input)
+imgui.ColorConvertHSVtoRGB(h, s, v, out_r, out_g, out_b)
+imgui.ColorConvertRGBtoHSV(r, g, b, out_h, out_s, out_v)
+imgui.ColorConvertU32ToFloat4(input)
