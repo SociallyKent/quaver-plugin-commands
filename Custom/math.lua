9bb5fb7322696a472897c4e8643caedf33ef58c6
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-04-10 21:01:32 -0500

    Added some stuff

--- Custom/math.lua
+++ Custom/math.lua
@@ -3,13 +3,22 @@
 	m, n, o, p, q, r,
 	s, t, u, v, w, x,
 	y, z
+ -- Restrict a NUMBER's value between LOW and HIGH
+--NUMBER numeric
+--LOW numeric
+--HIGH numeric
+function math.clamp(NUMBER, LOW, HIGH)
+	return NUMBER < LOW and LOW or NUMBER > HIGH and HIGH or NUMBER end
+ -- Returns both the min and max of given numbers
+--... numeric
+function math.minmax(...)
+	return math.min(...), math.max(...) end
  -- Round NUMBER to decimal point DECIMAL
 --NUMBER numeric
 --DECIMAL numeric
-function math.round(NUMBER, DECIMAL)
-	DECIMAL = DECIMAL or 0
-	local Notation = 10^DECIMAL
-	return math.floor(Notation * NUMBER+0.5)/ Notation--+0.5; for not round downwards
+function math.round(NUM, DECIMAL)
+	local Notation = 10^(DECIMAL or 0)
+	return math.floor(Notation * NUM+0.5)/Notation end--+0.5; for not round downwards
 end
  -- Wrap NUMBER1 until it fits within NUMBER2
 --NUMBER1 numeric
