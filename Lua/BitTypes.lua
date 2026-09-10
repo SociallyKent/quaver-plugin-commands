20dfa9374e0236ba9b32e349ad5e8b825bb90730
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-09-10 17:06:11 -0500

    Created some comments on those I know

---
+++ Lua/BitTypes.lua
@@ -0,0 +1,22 @@
+ -- Arithmetic bitwise-shift x right (by the bit count of) y
+bit32.arshift(x, y)
+ --The bitwise-and of all its arguments
+bit32.band(x [, x1, ...])
+ --Bitwise-not of x
+bit32.bnot(x)
+ --The bitwise-or of all its arguments
+bit32.bor(x [, x1, ...])
+ --The bitwise-xor of all its arguments
+bit32.bxor(x [, x1, ...])
+ --get bitwise 'and' and if different from zero.
+bit32.btest(x [, x1, ...])
+bit32.extract(x, field, ¿width)
+bit32.replace(x, y, field, ¿width)
+ --bit shift x rotated left by y
+bit32.lrotate(x, y)
+ --bit rotate x right by y
+bit32.rrotate(x, y)
+ --Logical bitwise-shift x left by (the bit count of) y
+bit32.lshift(x, y)
+ --Logical bitwise-shift x right by (the bit count of) y
+bit32.rshift(x, y)
